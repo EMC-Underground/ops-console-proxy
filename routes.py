8f5a911d1c3c14d8177924117144da5e2e0c259b
JP Quicksall <john.paul.quicksall@gmail.com>
2016-06-02 15:46:38 -0700

    Moved the initial updates to urls

--- routes.py
+++ routes.py
@@ -18,15 +18,24 @@ port = int(os.getenv('VCAP_APP_PORT', 8080))
 def hello_world():
   return 'Hello World!'
 
+@app.route('/update/installs/', methods=['PUT'])
+def update_installs():
+  methods.refresh_installs()
+  return 'OK'
+
+@app.route('/update/srs/', methods=['PUT'])
+def update_srs():
+  methods.refresh_srs()
+  return 'OK
+
+
 # Start App
 if __name__ == '__main__':
   scheduler.add_job(methods.refresh_installs, 'interval', hours=24)
   scheduler.add_job(methods.refresh_srs, 'interval', minutes=15)
   scheduler.add_listener(methods.error_listener, events.EVENT_JOB_EXECUTED | events.EVENT_JOB_ERROR)
   scheduler.start()
-  methods.refresh_installs()
-  methods.refresh_srs()
-  
+
   try:
     app.run(host='0.0.0.0', port=port)
 
